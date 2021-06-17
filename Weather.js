75b6bc5f8be96a04295588bdaa6771ab792a79a5
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2021-06-17 15:04:01 +0900

    title and subtitle

--- Weather.js
+++ Weather.js
@@ -8,42 +8,62 @@ const weatherOptions = {
   Haze: {
     iconName: "weather-hazy",
     gradient: ["#3B4371", "#F3904F"],
+    title: "",
+    subtitle: "",
   },
   Thunderstorm: {
     iconName: "weather-lightning",
-    gradient: ["#1e130c", "#9a8478"],
+    gradient: ["#373B44", "#4286f4"],
+    title: "",
+    subtitle: "",
   },
   Drizzle: {
     iconName: "weather-partly-rainy",
     gradient: ["#DBE6F6", "#C5796D"],
+    title: "",
+    subtitle: "",
   },
   Rain: {
     iconName: "weather-pouring",
     gradient: ["#4CA1AF", "#C4E0E5"],
+    title: "Rain",
+    subtitle: "밖에비온다주륵주륵",
   },
   Snow: {
     iconName: "weather-snowy-heavy",
-    gradient: ["#DAE2F8", "#D6A4A4"],
+    gradient: ["#9796f0", "#fbc7d4"],
+    title: "Snow",
+    subtitle: "❄️❄️❄️❄️❄️⛄️❄️❄️❄️❄️❄️",
   },
   Atmosphere: {
     iconName: "weather-tornado",
     gradient: ["#5D4157", "#A8CABA"],
+    title: "이상한 날씨",
+    subtitle: "꾸물꾸물",
   },
   Clear: {
     iconName: "weather-sunny",
     gradient: ["#ED4264", "#FFEDBC"],
+    title: "Clear",
+    subtitle: "드라이브 하러 🏍",
   },
   Clouds: {
     iconName: "weather-cloudy",
-    gradient: ["#ECE9E6", "#FFFFFF"],
+    gradient: ["#DBE6F6", "#C5796D"],
+    title: "Clouds",
+    subtitle: "비는 안오겠쥬",
   },
   Dust: {
     iconName: "weather-tornado",
     gradient: ["#1F1C2C", "#928DAB"],
+    title: "Dust",
+    subtitle: "마스크 필수",
   },
   Mist: {
     iconName: "weather-fog",
     gradient: ["#0ABFBC", "#FC354C"],
+    title: "Mist",
+    subtitle: "운전 조심하세요",
   },
 };
 
@@ -63,7 +83,12 @@ export default function Weather({ temp, condition }) {
         />
         <Text style={styles.temp}>{temp}º</Text>
       </View>
-      <View style={styles.halfContainer}></View>
+      <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
     </LinearGradient>
   );
 }
@@ -97,5 +122,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 24,
+  },
+  textContainer: {
+    paddingHorizontal: 20,
+    alignItems: "flex-start",
   },
 });
